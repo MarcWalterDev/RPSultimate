4892ab832b5dfada5f0d43f1f6bb9655d6476362
Marc Walter <marcwalter0@gmail.com>
2024-06-07 17:06:13 +0000

    win conditions function created

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,6 +16,22 @@ function playGame(playerChoice) {
 			let selection = this.id;
 			let compChoice = choices[Math.floor(Math.random() * choices.length)];
 			input.innerHTML = `computer chose: ${compChoice}  |  you chose: ${selection}`;
+
+			winner(selection, compChoice);
 		});
 	});
+}
+
+function winner(player, computer) {
+	if (player === computer) {
+		console.log("it's a draw");
+	} else if (
+		(player === "rock" && computer === "scissors") ||
+		(player === "paper" && computer === "rock") ||
+		(player === "scissors" && computer === "paper")
+	) {
+		console.log("you win");
+	} else {
+		console.log("loser!");
+	}
 }
